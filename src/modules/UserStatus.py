529ee2412c534d062b87f8201624a3cb369e0722
Night Key <davidjantho@gmail.com>
2026-07-19 10:37:22 +0200

    Fixed unhashable type error
    Signed-off-by: Night Key <davidjantho@gmail.com>

--- src/modules/UserStatus.py
+++ src/modules/UserStatus.py
@@ -15,6 +15,9 @@ class UserStatus(Enum):
     def __ne__(self, other):
         return not self == other
 
+    def __hash__(self):
+        return hash(self.name)
+
     @staticmethod
     def from_string(string: str) -> 'UserStatus':
         if string == "quiet": return UserStatus.Quiet
